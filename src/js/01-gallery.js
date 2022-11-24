e43ef75a7e3ce96dd64a99bea9143c32c0b95bfc
Sanderrica <99506840+Sanderrica@users.noreply.github.com>
2022-11-24 19:26:07 +0100

    Task 1, 2, 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const markup = galleryItems.map(({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>`).join('');
+
+const galleryList = document.querySelector('.gallery');
+
+galleryList.insertAdjacentHTML("afterbegin", markup);
+
+galleryList.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return
+    }
+
+    // ----SimpleLightbox----
+
+
+    `<div class="gallery">
+    <a href="${event.target.getAttribute(
+      "href"
+    )}"><img src="${event.target.getAttribute(
+    "src"
+  )}" alt="${event.target.getAttribute("alt")}" /></a>
+    <a href="${event.target.getAttribute(
+      "href"
+    )}"><img src="${event.target.getAttribute(
+    "src"
+  )}" alt="${event.target.getAttribute("alt")}"/></a>
+   </div>
+    `; 
+};
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  showCounter: false,
+});
+
+gallery.on("show.simplelightbox");
 
 console.log(galleryItems);
